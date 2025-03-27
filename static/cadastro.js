9b85a5e6cdb25f7b9e9e51945aab889547e916d3
poledna0 <henriquepoledna@proton.me>
2025-03-27 01:32:29 +0000

    add js and rm rust

---
+++ static/cadastro.js
@@ -0,0 +1,18 @@
+function vr_cd(){
+    let vnome = document.getElementById("inome").value;
+    let vemail = document.getElementById("iemail").value;
+    let vcpf = document.getElementById("icpf").value;
+    let vdata_nascimento = document.getElementById("idata_nascimento").value;
+    let vtelefone = document.getElementById("itelefone").value;
+    let vsenha = document.getElementById("isenha").value;
+    let vconfirmar_senha = document.getElementById("iconfirmar_senha").value;
+    let vrua = document.getElementById("irua").value;
+    let vnumero = document.getElementById("inumero").value;
+    let vcomplemento = document.getElementById("icomplemento").value;
+    let vbairro = document.getElementById("ibairro").value;
+    let vcidade = document.getElementById("icidade").value;
+    let vestado = document.getElementById("iestado").value;
+    let vcep = document.getElementById("icep").value;
+    let vgenero = document.getElementById("igenero").value;
+
+}
