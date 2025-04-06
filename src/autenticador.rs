88a722aafd76ce60130d9f4f66b3d6bc6a9659a3
poledna0 <henriquepoledna@proton.me>
2025-04-06 16:02:59 -0300

    arrumei a funcao de gerar os 6 digitos do MFA

--- src/autenticador.rs
+++ src/autenticador.rs
@@ -1,40 +1,18 @@
 use koibumi_base32 as base32;
-use std::io::{self, Write};
 use std::time::SystemTime;
 use totp_lite::{totp_custom, Sha1, DEFAULT_STEP};
 
-pub fn valida_codigo_autenticador() {
-    println!("Press ctrl-c to cancel.");
-    loop {
-        let mut input = String::new();
+pub fn valida_codigo_autenticador(codigo: String) -> String {
+    let seconds: u64 = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
 
-        print!("Enter your TOTP secret: ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut input).unwrap();
-
-        let length = input.trim().len();
-        if length != 16 && length != 26 && length != 32 {
-            println!("Invalid TOTP secret, must be 16, 26 or 32 characters.");
-            continue;
-        }
-
-        let seconds: u64 = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-
-        println!(
-            "Your TOTP code: {}",
-            totp_custom::<Sha1>(
-                // Calculate a new code every 30 seconds.
-                DEFAULT_STEP,
-                // Calculate a 6 digit code.
-                6,
-                // Convert the secret into bytes using base32::decode().
-                &base32::decode(&input.trim().to_lowercase()).unwrap(),
-                // Seconds since the Unix Epoch.
-                seconds,
-            )
-        );
-    }
+    // Gera e retorna o código TOTP baseado no segredo e tempo
+    totp_custom::<Sha1>(
+        DEFAULT_STEP, // Intervalo de 30 segundos
+        6,            // Código de 6 dígitos
+        &base32::decode(&codigo.trim().to_lowercase()).unwrap(),
+        seconds,      // Tempo atual em segundos
+    )
 }
