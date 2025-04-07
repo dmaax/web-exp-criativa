a0bd2c28eff5bd9d82dd86e70618e4540335e223
poledna0 <henriquepoledna@proton.me>
2025-04-06 23:06:31 -0300

    tudo funcionando (eu acho)

--- static/register.js
+++ static/register.js
@@ -144,4 +144,20 @@ async function validarCadastro() {
         });
     }
 }
-    
+
+function login(event) {
+    event.preventDefault();
+  
+    const nome = document.getElementById("userId").value;
+    const senha = document.getElementById("password").value;
+  
+    if (nome === "admin@pucpr.edu.br" && senha === "admin") {
+      window.location.href = "/static/coloca_codigo.html";
+    } else {
+      alert("Usuário ou senha incorretos");
+    }
+  
+    return false;
+  }
+  
+  
