9851ab8ee988f986ebd410124f29266bba346472
poledna0 <henriquepoledna@proton.me>
2025-04-09 09:42:12 -0300

    arrumando erros, html e responsividade

--- static/login.js
+++ static/login.js
@@ -1,3 +1,18 @@
 document.getElementById("register-tab").addEventListener("click", function () {
     window.location.href = "/static/register_page.html";
 });
+
+function login(event) {
+    event.preventDefault();
+  
+    const nome = document.getElementById("userId").value;
+    const senha = document.getElementById("password").value;
+  
+    if (nome === "admin@pucpr.edu.br" && senha === "admin") {
+      window.location.href = "/static/coloca_codigo.html";
+    } else {
+      alert("Usuário ou senha incorretos");
+    }
+  
+    return false;
+}
--- static/register.js
+++ static/register.js
@@ -154,21 +154,7 @@ async function validarCadastro() {
             body: JSON.stringify({ email: email, nome: nome }),
         });
     }
-}
-
-function login(event) {
-    event.preventDefault();
-  
-    const nome = document.getElementById("userId").value;
-    const senha = document.getElementById("password").value;
-  
-    if (nome === "admin@pucpr.edu.br" && senha === "admin") {
-      window.location.href = "/static/coloca_codigo.html";
-    } else {
-      alert("Usuário ou senha incorretos");
-    }
-  
-    return false;
+    window.location.href = "/static/login_page.html";
 }
 
 
