af0e55008f1ed5d50527e6a23e3bcef86dd42588
poledna0 <henriquepoledna@proton.me>
2025-04-17 21:29:51 -0300

    uma maravilha da computacao, mas n testei ainda

--- src/autenticador.rs
+++ src/autenticador.rs
@@ -6,10 +6,10 @@ use rocket::serde::{Deserialize, json::Json};
 
 #[derive(Debug, Deserialize)]
 pub struct CodigoMfa {
-    pub codigo: String,
+    pub codigo: Box<str>,
 }
 
-pub fn valida_codigo_autenticador(codigo: String) -> String {
+pub fn valida_codigo_autenticador(codigo: &str) -> Box<str> {
     let seconds: u64 = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
@@ -19,18 +19,18 @@ pub fn valida_codigo_autenticador(codigo: String) -> String {
         6,
         &base32::decode(&codigo.trim().to_lowercase()).unwrap(),
         seconds,
-    )
+    ).into()
 }
 
 #[post("/verifica_mfa", format = "json", data = "<entrada_codigo>")]
-pub async fn vcod(entrada_codigo: Json<CodigoMfa>) -> Result<String, Status> {
-    let x = "ea273b66in5pvp64sg2gigpwuu".to_string();
+pub async fn vcod(entrada_codigo: Json<CodigoMfa>) -> Result<Json<bool>, Status> {
+    let x: &str = "ea273b66in5pvp64sg2gigpwuu";
 
-    let saida_codigo: String = valida_codigo_autenticador(x);
+    let saida_codigo: Box<str> = valida_codigo_autenticador(&x);
 
-    if entrada_codigo.codigo.trim() == saida_codigo {
-        Ok("true".to_string())
+    if entrada_codigo.codigo.trim() == &*saida_codigo{
+        Ok(Json(true))
     } else {
-        Ok("false".to_string())
+        Ok(Json(false))
     }
 }
