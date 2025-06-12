a901ffbf095befbf6f6b1a23135171c9ec1c2791
poledna0 <henriquepoledna@proton.me>
2025-06-12 23:26:43 +0000

    rm comentario

--- src/chave.rs
+++ src/chave.rs
@@ -23,5 +23,3 @@ pub fn obter_chave_privada() -> String {
     dotenv().ok();
     std::env::var("CHAVE_PRIVADA").unwrap_or_else(|_| "chave_nao_encontrada".to_string())
 }
-
-//CHAVE_PUBLICA="-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlE6F/CmyUR6JXafRZ+G8\nRz1P0Ij2I8pokVvEn85y/r1z5SFsTQPtlqA202sm0VkwHgMi+cK2CoD+E7YyfTWU\nWxtFj+WRPiyq/2NYfPGLxGQMOUrlxZWSMWzThdw6MM901YA2wzirMQaVu/mWX17m\n3tweQi2AQgMtRaT8WOzmBjNd3iaA8UHifEBC98yzEt5ld0pQi6YpqluQx2aK5L3C\nFrs5j/zKZoZakxU2RDlvZKfwmxm6VUpHl32Ac0sGTOsWZe1V0QYKz7+0ckE7nu1u\nZul+VrBmdRBTviNnOaGXLei3f+hN4+AYtuBdu4TOCOQ904nxGpsiXJVhPDk5tHVx\nNQIDAQAB\n-----END PUBLIC KEY-----"
